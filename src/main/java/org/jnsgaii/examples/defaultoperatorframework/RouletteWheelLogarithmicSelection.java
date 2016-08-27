cd5245e3d2ce0e890d31df8add7fc33cd14eff66
Mitchell Skaggs <skaggsm333@gmail.com>
2016-08-27 11:33:37 -0500

    Fixed logarithmic selection log(0) bug

--- src/main/java/org/jnsgaii/examples/defaultoperatorframework/RouletteWheelLogarithmicSelection.java
+++ src/main/java/org/jnsgaii/examples/defaultoperatorframework/RouletteWheelLogarithmicSelection.java
@@ -6,8 +6,9 @@ import org.apache.commons.math3.util.FastMath;
  * Created by Mitchell on 4/22/2016.
  */
 public class RouletteWheelLogarithmicSelection<E> extends RouletteWheelSelection<E> {
+
     @Override
     protected double getWeight(int rank) {
-        return FastMath.log(rank);
+        return FastMath.log(rank + 1);
     }
 }
