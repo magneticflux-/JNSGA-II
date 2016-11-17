e27dd2aa2cd6dfb81a0f4c46686d98486fc68f8e
Mitchell Skaggs <skaggsm333@gmail.com>
2016-11-17 10:01:20 -0600

    Optimized use of reflection in UpdatableHistogramDataset

--- src/main/java/org/jnsgaii/UpdatableHistogramDataset.java
+++ src/main/java/org/jnsgaii/UpdatableHistogramDataset.java
@@ -3,22 +3,31 @@ package org.jnsgaii;
 import org.jfree.data.statistics.HistogramDataset;
 
 import java.lang.reflect.Field;
-import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by skaggsm on 3/29/16.
  */
 public class UpdatableHistogramDataset extends HistogramDataset {
 
+    private Field list;
+
     public void removeAllSeries() {
-        try {
-            Field list = HistogramDataset.class.getDeclaredField("list");
+        if (list == null) {
+            try {
+                list = HistogramDataset.class.getDeclaredField("list");
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            }
             list.setAccessible(true);
-            ((Collection) list.get(this)).clear();
-            fireDatasetChanged();
-        } catch (IllegalAccessException | NoSuchFieldException e) {
-            e.printStackTrace();
         }
+        if (list != null)
+            try {
+                ((List) list.get(this)).clear();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        fireDatasetChanged();
     }
 
     public UpdatableHistogramDataset clone() throws CloneNotSupportedException {
